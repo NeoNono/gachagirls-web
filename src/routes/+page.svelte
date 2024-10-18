ed30bcbef66f86c0a4bb7871ad1f62de6e8ae9a5
NeoNono <yuliashkekina@gmail.com>
2024-10-18 22:30:18 +0200

    added basic api connection

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,12 @@
+<script>
+    import { onMount } from "svelte";
+    export let data;
+    $: response = "please wait"
+    onMount(async () => {
+        response = await data.result;
+    })
+</script>
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+{@html response}
